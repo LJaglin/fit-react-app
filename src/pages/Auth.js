3b4a62fb3ac5fdab95fd514055a0895fe854f29d
LJaglin <ljaglinski@o2.pl>
2020-09-09 13:10:16 +0200

    Add submitHandler

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -3,16 +3,58 @@ import React, { Component } from 'react';
 import './Auth.css';
 
 class AuthPage extends Component {
+    constructor(props) {
+        super(props);
+        this.emailEl = React.createRef();
+        this.passwordEl = React.createRef();
+    }
+
+    submithandler = event => {
+        event.preventDefault();
+        const email = this.emailEl.current.value;
+        const password = this.passwordEl.current.value;
+
+        if (email.trim().length === 0 || password.trim().length === 0) {
+            return;
+        }
+
+        const data = {
+            email: email,
+            password: password
+        };
+
+        fetch('http://localhost:8080/user/addUser', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(res => {
+            if (res.status !== 200 && res.status !== 201) {
+                throw new Error('Failed!');
+            }
+            return res.json();
+        })
+        .then(resData => {
+            console.log(resData);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+
+    };
+
     render() {
         return (
-            <form className="auth-form">
+            <form className="auth-form" onSubmit={this.submithandler}>
                 <div className="form-control">
                     <label htmlFor="email">E-Mail</label>
-                    <input type="email" id="email" />
+                    <input type="email" id="email" ref={this.emailEl}/>
                 </div>
                 <div className="form-control">
                     <label htmlFor="password">Password</label>
-                    <input type="password" id="password" />
+                    <input type="password" id="password" ref={this.passwordEl}/>
                 </div>
                 <div className="form-actions">
                     <button type="submit">Submit</button>
